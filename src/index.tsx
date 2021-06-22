4e0d48912f870d0a1c955cb222075a60e441dcca
Shawn Stawiarski <contact@shawnstawiarski.com>
2021-06-22 00:30:19 -0400

    fix: update store import

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { store } from './app/store';
+import { store } from './redux/store'
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import GlobalStyles from './styles/GlobalStyles';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+      <GlobalStyles />
       <App />
     </Provider>
   </React.StrictMode>,
