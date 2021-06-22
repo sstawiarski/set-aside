dd5b06423d7584f83ff53727c3668d7942923fa0
Shawn Stawiarski <contact@shawnstawiarski.com>
2021-06-22 13:19:41 -0400

    feat: add media query hook

---
+++ src/hooks/useMediaQuery.ts
@@ -0,0 +1,23 @@
+/* Adapted from https://www.netlify.com/blog/2020/12/05/building-a-custom-react-media-query-hook-for-more-responsive-apps/ */
+
+import { useState, useEffect } from 'react';
+
+const useMediaQuery = (query: string) => {
+  const [matches, setMatches] = useState(false);
+
+  useEffect(() => {
+    const media = window.matchMedia(query);
+    if (media.matches !== matches) {
+      setMatches(media.matches);
+    }
+    const listener = () => {
+      setMatches(media.matches);
+    };
+    media.addEventListener('change', listener);
+    return () => media.removeEventListener('change', listener);
+  }, [matches, query]);
+
+  return matches;
+};
+
+export default useMediaQuery;
